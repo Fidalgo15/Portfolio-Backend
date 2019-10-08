c46b2fd29dc8128b7e78009b81557d6bafd0b340
Devin Sherman <fidalgo15@Devins-MacBook-Pro.local>
2019-10-07 23:19:46 -0600

    skills.js created

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ let  logger = require('morgan');
 let  indexRouter = require('./routes/index');
 let  usersRouter = require('./routes/users');
 let loginRouter = require('./routes/Login');
-let workRouter = require('./routes/WorkExperience')
+let workRouter = require('./routes/WorkExperiences');
+let skillsRouter = require('./routes/Skills');
 
 let  app = express();
 
@@ -24,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
-app.use('/work', workRouter)
+app.use('/work', workRouter);
+app.use('/skills', skillsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/Skills.js
@@ -0,0 +1,20 @@
+let  express = require('express');
+let  router = express.Router();
+
+const dummySkill = {
+    _id: "123",
+    name: "javascript",
+    rank: "10"
+}
+
+router.get('/', (req, res, next) => res.json({dummySkill}))
+
+router.get('/:id', (req, res, next) => res.json({dummySkill}))
+
+router.post('/', (req, res,next) => res.json({dummySkill}))
+
+router.put('/:id', (req, res, next) => res.json({dummySkill}))
+
+router.delete('/:id', (req, res, next) => res.json({dummySkill}))
+
+module.exports = router;
